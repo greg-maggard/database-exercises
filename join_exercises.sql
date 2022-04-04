5f99ff671aaa96c4aeba1d587b77fe7989dc6d84
Greg Maggard <gregamaggard@gmail.com>
2022-04-03 22:17:15 -0500

    Started the exercises, but haven't done much yet. Need to review this portion.

--- join_exercises.sql
+++ join_exercises.sql
@@ -1,1 +1,8 @@
 use join_example_db;
+
+
+-- Use the join_example_db. Select all the records from both the users and roles tables.
+SELECT * FROM users;
+SELECT * FROM roles;
+
+-- Use join, left join, and right join to combine results from the users and roles tables as we did in the lesson. Before you run each query, guess the expected number of results.
