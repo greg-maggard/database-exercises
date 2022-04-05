d4677a78687168dac1b0d58637657fe73d4b329d
Greg Maggard <gregamaggard@gmail.com>
2022-04-05 13:32:01 -0500

    Worked through these exercises as a class, but I shoudl still go back and rework them.

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -2,35 +2,77 @@ USE employees;
 
 -- Find all the current employees with the same hire date as employee 101010 using a sub-query.
 
-SELECT first_name, last_name
-FROM employees
-WHERE hire_date = (SELECT hire_date FROM employees WHERE emp_no = '101010');
+SELECT 
+    first_name, last_name
+FROM
+    employees
+WHERE
+    to_date > NOW() -- COME BACK AND FIX THIS LINE
+        AND hire_date = (SELECT 
+            hire_date
+        FROM
+            employees
+        WHERE
+            emp_no = '101010'); 
 -- This query returns 69 rows. 
 
 -- Find all the titles ever held by all current employees with the first name Aamod.
-SELECT DISTINCT title
-FROM titles
-WHERE emp_no IN (SELECT emp_no FROM employees WHERE first_name = 'Aamod') AND to_date LIKE '9999%';
+SELECT DISTINCT
+    title
+FROM
+    titles
+WHERE
+    emp_no IN (SELECT 
+            emp_no
+        FROM
+            employees
+        WHERE
+            first_name = 'Aamod')
+        AND to_date LIKE '9999%';
 
 -- How many people in the employees table are no longer working for the company? Give the answer in a comment in your code.
-SELECT COUNT(*)
-FROM employees
-WHERE emp_no NOT IN (SELECT DISTINCT emp_no FROM dept_emp WHERE to_date > curdate());
+SELECT 
+    COUNT(*)
+FROM
+    employees
+WHERE
+    emp_no NOT IN (SELECT DISTINCT
+            emp_no
+        FROM
+            dept_emp
+        WHERE
+            to_date > CURDATE());
 
 -- There are 59900 people that are no longer with the company. 
 
 -- Find all the current department managers that are female. List their names in a comment in your code.
-SELECT CONCAT(first_name, ' ', last_name) as 'Name'
-FROM employees
-WHERE emp_no IN (SELECT emp_no FROM dept_manager WHERE gender = 'F' AND to_date > curdate());
+SELECT 
+    CONCAT(first_name, ' ', last_name) AS 'Name'
+FROM
+    employees
+WHERE
+    emp_no IN (SELECT 
+            emp_no
+        FROM
+            dept_manager
+        WHERE
+            gender = 'F' AND to_date > CURDATE()
+            );
 -- Isamu Legleitner, Larsten Sigstam, Leon DasSarma, Hilary Kambil
 
 -- Find all the employees who currently have a higher salary than the companies overall, historical average salary.
-SELECT CONCAT(first_name, ' ', last_name) as 'Name'
-FROM employees 
-JOIN salaries
-	ON employees.emp_no = salaries.emp_no
-WHERE to_date LIKE '9999%' AND salary > (SELECT AVG(salary) FROM salaries);
+SELECT 
+    CONCAT(first_name, ' ', last_name) AS 'Name'
+FROM
+    employees
+        JOIN
+    salaries ON employees.emp_no = salaries.emp_no
+WHERE
+    to_date LIKE '9999%'
+        AND salary > (SELECT 
+            AVG(salary)
+        FROM
+            salaries);
 -- 150,000+ results from this query when run. 
 
 -- How many current salaries are within 1 standard deviation of the current highest salary? (Hint: you can use a built in function to calculate the standard deviation.) What percentage of all salaries is this?
@@ -38,11 +80,23 @@ WHERE to_date LIKE '9999%' AND salary > (SELECT AVG(salary) FROM salaries);
 SELECT MAX(salary) - STDDEV(salary) FROM salaries
 WHERE to_date > NOW();
 
-SELECT COUNT(*), COUNT(*) / (SELECT COUNT(*) FROM salaries WHERE to_date > NOW()) * 100
+SELECT COUNT(*) as 'Number of Salaries', COUNT(*) / (SELECT COUNT(*) FROM salaries WHERE to_date > NOW()) * 100 as 'Percentage'
 FROM salaries
 WHERE to_date > NOW() AND salary > (SELECT MAX(salary) - STDDEV(salary) FROM salaries
 WHERE to_date > NOW());
 -- There are are 83 people with salaries within 1 SD of the current highest salary.
 
+-- Workign through solution:
+-- What is the max salary
+SELECT max(salary) from salaries where to_date > now();
 
+-- What is 1 SD for current salary?
+SELECT STDDEV(salary) from salaries where to_date > now();
+
+SELECT COUNT(*) from salaries
+where to_date > now()
+and salary > (SELECT max(salary) from salaries where to_date > now()) - (SELECT STDDEV(salary) from salaries where to_date > now());
+
+# What percentage? 
+# count of numerator/count of overall (denominator) * 100
 
