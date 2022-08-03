0211394bdbb51f030183b32d7a4a7fa932fa8f1f
Greg Maggard <gregamaggard@gmail.com>
2022-08-02 23:09:07 -0500

    Replaced more_exercises.sql with later version.

--- more_exercises.sql
+++ more_exercises.sql
@@ -1,102 +1,185 @@
-##More Drills With The Sakila DatabaseMore Drills With The Sakila Database##
-USE sakila; 
+# Drills with Sakila DB
 
-# 1. SELECT Statements
--- a. Select all columns from the actor table.
-SELECT * 
-FROM actor;
--- b. Select only the last_name column from the actor table.
-SELECT last_name 
-FROM actor;
--- c. Select only the film_id, title, and release_year columns from the film table.
-SELECT film_id, title, release_year
-FROM film;
+USE sakila;
 
-# 2. DISTINCT Operator
--- a. Select all distinct (different) last names from the actor table.
-SELECT DISTINCT last_name
-FROM actor;
--- b. Select all distinct (different) postal codes from the address table.
-DESCRIBE address;
+# 1. SELECT Statements:
 
-SELECT DISTINCT postal_code
-FROM address;
+SELECT * FROM actor;
 
--- c. Select all distinct (different) ratings from the film table.
-DESCRIBE film;
+SELECT last_name FROM actor;
 
-SELECT DISTINCT rating
-FROM film;
+SELECT film_id, title, release_year FROM film;
 
-#3. WHERE Clause
--- a. Select the title, description, rating, movie length columns from the films table that last 3 hours or longer.
-SELECT title, description, rating, length 
-FROM film
-WHERE length > 180;
-	#This returns 39 rows. 
-   
--- b. Select the payment id, amount, and payment date columns from the payments table for payments made on or after 05/27/2005.
-DESCRIBE payment;
+# 2. DISTINCT Operator:
+
+SELECT DISTINCT last_name FROM actor;
+
+SELECT DISTINCT postal_code FROM address;
+
+SELECT DISTINCT rating FROM film;
+
+# 3. WHERE clause:
+SELECT * FROM payment;
+
+SELECT title, description, rating, length FROM film
+WHERE length > 3;
+
+SELECT payment_id, amount, payment_date 
+FROM payment
+WHERE payment_date >= '2005-05-27';
+#I need to remember to put these in quotations, and follow the regular format.
 
 SELECT payment_id, amount, payment_date
 FROM payment 
-WHERE payment_date > '2005-5-27';
-	#This returns 15,730 rows.
-  
--- c. Select the primary key, amount, and payment date columns from the payment table for payments made on 05/27/2005.
-DESCRIBE payment;
-	# payment_id is the primary key
-
-SELECT payment_id, amount, payment_date
-FROM payment 
-WHERE payment_date > '2005-5-27';
-	#This is the same as the above, I just used DESCRIBE to verify the primary key before writing the SELECT statement. 
-
--- d. Select all columns from the customer table for rows that have a last names beginning with S and a first names ending with N.
-DESCRIBE customer;
-
-SELECT * 
-FROM customer 
-WHERE last_name LIKE 'S%N';
-	#This returns 6 rows. 
-
--- e. Select all columns from the customer table for rows where the customer is inactive or has a last name beginning with "M".
-SELECT * 
-FROM customer 
-WHERE active = 0 OR last_name LIKE 'M%';
-	#This returns 72 rows.
-    
--- f. Select all columns from the category table for rows where the primary key is greater than 4 and the name field begins with either C, S or T.
-DESCRIBE category;
+WHERE payment_date = '2005-05-27';
+#There are no payments on 5/27/2005.
 
 SELECT *
-FROM category 
-WHERE category_id > 4 
-AND name LIKE 'C%' OR name LIKE 'S%' or name LIKE 'T%';
-	#This returns 4 rows.
-    
--- g. Select all columns minus the password column from the staff table for rows that contain a password.
-DESCRIBE staff;
+FROM customer
+WHERE last_name LIKE 'S%' AND first_name LIKE '%N';
 
-SELECT address_id, picture, email, store_id, active, username, last_update
+select * from customer;
+
+SELECT * 
+FROM customer 
+WHERE active = 0 
+OR last_name LIKE 'M%';
+
+SELECT * FROM category;
+
+SELECT *
+FROM category
+WHERE category_id > 4
+AND 
+name LIKE 'C%' 
+OR name LIKE 'S%' 
+OR name LIKE 'T%';
+
+SELECT staff_id, first_name, last_name, address_id, picture, email, store_id, active, username, last_update
 FROM staff
 WHERE password IS NOT NULL;
-	#This returns one employee.
 
--- h. Select all columns minus the password column from the staff table for rows that do not contain a password.
-SELECT address_id, picture, email, store_id, active, username, last_update
+SELECT staff_id, first_name, last_name, address_id, picture, email, store_id, active, username, last_update
 FROM staff
 WHERE password IS NULL;
-	#This returns the one other employee. 
-    
-#4. IN Operator
--- a. Select the phone and district columns from the address table for addresses in California, England, Taipei, or West Java.
-DESCRIBE address;
-	#The 'district' column is the relevant field. 
-SELECT phone, district 
-FROM address 
+#Another method to solve this would have been to create a temp table of all columns and then drop the password column, but I needed permissions. 
+
+# IN Operator:
+
+SELECT phone, district
+FROM address
 WHERE district IN ('California', 'England', 'Taipei', 'West Java');
-	#This returns 42 rows. 
-    
+
+SELECT payment_id, amount, payment_date
+FROM payment
+WHERE DATE(payment_date) IN ('2005-05-25', '2005-05-27', '2005-05-29');
+# When I was originally doing this, I was trying to put the date on the 'IN' list values, but I needed to pull the date from the table's datetime values. 
+
+SELECT *
+FROM film
+WHERE rating in ('G', 'PG-13', 'NC-17');
+
+#BETWEEN Operator:
+
+SELECT *
+FROM payment
+WHERE payment_date BETWEEN '2005-05-25' and '2005-05-26'
+ORDER BY payment_date;
+#Using ORDER BY to ensure I grabbed all the write rows.
+
+SELECT * FROM film;
+
+SELECT film_id, title, description
+FROM film 
+WHERE LENGTH(description) BETWEEN 100 AND 120;
+
+#LIKE Operator:
+
+SELECT *
+FROM film
+WHERE description LIKE 'A Thoughtful%'; 
+
+SELECT *
+FROM film
+WHERE description LIKE '%Boat';
+
+SELECT *
+FROM film
+WHERE description LIKE ('%Database%')
+AND length > 180;
+
+#LIMIT Operator:
+
+SELECT *
+FROM payment
+LIMIT 20;
+
+SELECT * FROM payment;
+##LIMIT B########################################################################################
+SELECT payment_date, amount
+FROM payment
+WHERE amount > 5;
+#How do I select indices from the result set? It wants rows where indices in result set are between 1000 and 2000.
+
+##LIMIT C########################################################################################
+SELECT *
+FROM customer;
+#How do I select indices from the result set? It wants rows where indices in result set are between 101-200.alter
+
+#ORDER BY Statement:
+
+SELECT * 
+FROM film
+ORDER BY length ASC;
+
+SELECT DISTINCT rating
+FROM film 
+ORDER BY rating DESC;
+
+SELECT payment_date, amount
+FROM payment
+ORDER BY amount DESC
+LIMIT 20;
+
+SELECT * FROM film;
+
+SELECT title, description, special_features, length, rental_duration
+FROM film
+WHERE special_features LIKE '%Behind the Scenes%'
+AND length < 120
+AND rental_duration BETWEEN 5 and 7
+ORDER BY length DESC;
+
+# JOINs
+
+SELECT customer.first_name AS customer_first_name, customer.last_name AS customer_last_name, actor.first_name AS actor_first_name, actor.last_name AS actor_last_name
+FROM customer
+LEFT JOIN actor ON customer.last_name = actor.last_name;
+
+SELECT customer.first_name AS customer_first_name, customer.last_name AS customer_last_name, actor.first_name AS actor_first_name, actor.last_name AS actor_last_name
+FROM customer
+RIGHT JOIN actor on customer.last_name = actor.last_name;
+
+SELECT customer.first_name AS customer_first_name, customer.last_name AS customer_last_name, actor.first_name AS actor_first_name, actor.last_name AS actor_last_name
+FROM customer
+INNER JOIN actor on customer.last_name = actor.last_name;
+
+SELECT city, country
+FROM city
+LEFT JOIN country on city.country_id = country.country_id;
+
+SELECT * FROM language;
+
+SELECT title, description, language.name AS language_name
+FROM film
+LEFT JOIN language on film.language_id = language.language_id;
+
+SELECT * FROM city;
+
+SELECT first_name, last_name, address, address2, city.city AS city_name, district, postal_code
+FROM staff
+LEFT JOIN address ON staff.address_id = address.address_id
+LEFT JOIN city on address.city_id = city.city_id;
+
 
 
